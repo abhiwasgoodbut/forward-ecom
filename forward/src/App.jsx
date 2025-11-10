dc7225201141f86fa809ab7c08f5599b748740b3
abhiwasgoodbut <sabhinay427@gmail.com>
2025-11-10 14:33:27 +0530

    our first navbar is done

---
+++ forward/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Order from './pages/Order'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
+
+
+
+function App() {
+  
+    const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      <Route path='/collection' element={<Collection />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} />
+      <Route path='/product/:productId' element={<Product />} />
+      <Route path='/cart' element={<Cart />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/place-order' element={<PlaceOrder />} />
+      <Route path='/order' element={<Order />} />
+
+      </Route>
+    )
+  )
+
+  return (
+    <>
+      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]' >
+
+      <RouterProvider router={router} />
+
+      </div>
+    </>
+  )
+}
+
+export default App
