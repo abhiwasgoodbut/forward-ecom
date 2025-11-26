7a11e9290de2b97dcdb042865fdc75b010f92d1d
abhiwasgoodbut <sabhinay427@gmail.com>
2025-11-26 18:08:10 +0530

    fixing bugs

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,7 +1,7 @@
 import express from "express"
 
 import { addToCart, getUserCart,updateCart } from "../controllers/cartController.js"
-import { get } from "node-emoji"
+
 import authUser from "../middleware/auth.js"
 
 const cartRouter = express.Router()
