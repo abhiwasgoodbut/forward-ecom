30fbe5884748099e509347b3f4c8505b6a907304
abhiwasgoodbut <sabhinay427@gmail.com>
2025-11-26 20:47:54 +0530

    fixing bugs

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -43,28 +43,71 @@ function Navbar() {
 
         </ul>
 
-        <div className="flex items-center gap-6">
-            <img onClick={() => setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+       <div className="flex items-center gap-6">
+  <img
+    onClick={() => setShowSearch(true)}
+    src={assets.search_icon}
+    className="w-5 cursor-pointer"
+    alt=""
+  />
 
-            <div className="group relative">
-              <img onClick={()=> token? null : navigate('/login')} src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
-              {/* dropdown menu */}
-              {token && 
-              <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
-                <div className='flex flex-col gap-2 w-36 px-5 bg-slate-100 text-gray-500 rounded  '>
-                    <p className='cursor-pointer hover:text-black '>My Profile</p>
-                    <p onClick={()=>navigate("/order")} className='cursor-pointer hover:text-black '>Orders</p>
-                    <p onClick={logout} className='cursor-pointer hover:text-black '>Logout</p>
-                </div>
-            </div>}
-            </div>
-            <Link to='/cart' className='relative'>
-            <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
-            <p className='absolute right-[-5px] bottom-[-5px] w-4  text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>{getCartCount()}</p>
-            </Link>
-            <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
-            
+  {/* Profile Dropdown */}
+  <div className="relative">
+    {/* Hidden checkbox for mobile */}
+    <input type="checkbox" id="profile-toggle" className="peer hidden" />
+
+    {/* Profile icon */}
+    <label htmlFor="profile-toggle">
+      <img
+        onClick={() => (token ? null : navigate("/login"))}
+        src={assets.profile_icon}
+        className="w-5 cursor-pointer"
+        alt=""
+      />
+    </label>
+
+    {/* Dropdown menu */}
+    {token && (
+      <div
+        className="
+          hidden
+          peer-checked:block       /* mobile tap */
+          hover:block               /* desktop hover */
+          absolute right-0 pt-4 z-50
+        "
+      >
+        <div className="flex flex-col gap-2 w-36 px-5 bg-slate-100 text-gray-500 rounded shadow">
+          <p className="cursor-pointer hover:text-black">My Profile</p>
+          <p
+            onClick={() => navigate('/order')}
+            className="cursor-pointer hover:text-black"
+          >
+            Orders
+          </p>
+          <p onClick={logout} className="cursor-pointer hover:text-black">
+            Logout
+          </p>
         </div>
+      </div>
+    )}
+  </div>
+
+  {/* Cart Icon */}
+  <Link to="/cart" className="relative">
+    <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
+    <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+      {getCartCount()}
+    </p>
+  </Link>
+
+  {/* Mobile Menu */}
+  <img
+    onClick={() => setVisible(true)}
+    src={assets.menu_icon}
+    className="w-5 cursor-pointer sm:hidden"
+    alt=""
+  />
+</div>
 
         {/* Sidebar menu for small screens */}
         <div
